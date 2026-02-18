e0ade0bdf7ad71304eea1cf02c2f50832afe6bb9
Antti Pollanen <antti.pollanen89@gmail.com>
2026-02-18 14:40:31 +0200

    Finished ex02 and ex03

---
+++ ex02/incl/ScavTrap.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anpollan <anpollan@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 18:42:17 by anpollan          #+#    #+#             */
+/*   Updated: 2026/02/17 18:43:49 by anpollan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	public:
+		ScavTrap(void);
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap &other);
+		ScavTrap &operator=(const ScavTrap &other);
+		~ScavTrap(void);
+
+		void guardGate(void);
+};
---
+++ ex03/incl/FragTrap.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anpollan <anpollan@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 18:42:17 by anpollan          #+#    #+#             */
+/*   Updated: 2026/02/18 12:19:57 by anpollan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "ClapTrap.hpp"
+
+class FragTrap : virtual public ClapTrap
+{
+	public:
+		FragTrap(void);
+		FragTrap(std::string name);
+		FragTrap(const FragTrap &other);
+		FragTrap &operator=(const FragTrap &other);
+		~FragTrap(void);
+
+		void highFiveGuys(void);
+};
